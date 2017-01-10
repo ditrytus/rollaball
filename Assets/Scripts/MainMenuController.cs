6c4274264817f6a1ad028cc660c1b369ed9d5168
Jakub Gruszecki <jakubgruszecki@MacBook-Pro-Admin-2.local>
2017-01-10 21:56:35 +0100

    Added "MainMenu" screen

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+	public Button startButton;
+
+	public GameObject hudPanel;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+		startButton.onClick.AddListener(StartGame);
+	}
+	
+	void StartGame() {
+		Time.timeScale = 1;
+		gameObject.SetActive(false);
+		hudPanel.SetActive(true);
+	}
+
+}
